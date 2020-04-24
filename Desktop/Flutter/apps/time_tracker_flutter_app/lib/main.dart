e66543d509617eefe3d439c87aa9154e89938f7b
Laxmi Garde <laxmigarde95@gmail.com>
2020-04-24 22:54:02 +0530

    commit to add dart files

---
+++ Desktop/Flutter/apps/time_tracker_flutter_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/apps/sign_in/sign_in_page.dart';
+
+void main() {
+  runApp(MyApplication());
+}
+
+class MyApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter application',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SignInPage(),
+    );
+  }
+}
